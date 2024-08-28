3c82d43addb80848cbb3e71e2882d7e338b71dd8
Abhay Nimbalkar <abhaynimbalkar03@gmail.com>
2024-08-28 22:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+function scrollToContact() {
+    document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth' });
+}
 
+document.getElementById('mail-me-button').addEventListener('click', scrollToContact);
